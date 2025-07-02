00d5c045582da7737fd4294ec077db9bea271dd1
Pavankumarmarella <161482286+Pavankumarmarella99@users.noreply.github.com>
2025-07-02 11:43:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  li.addEventListener("click", function () {
+    li.classList.toggle("completed");
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(deleteBtn);
+  document.getElementById("taskList").appendChild(li);
+  input.value = "";
+}
